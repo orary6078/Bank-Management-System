ffa3da1be2f4e2d5bf577b1023f8eb67bfb04581
Ansari MD <103068419+RajaulAnsari@users.noreply.github.com>
2023-02-27 13:29:58 +0545

    Blink Bug Fixed, Rajaul Ansari

---
+++ BankManagementSystem/src/UserVerifier.java
@@ -0,0 +1,40 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+
+/**
+ *
+ * @author anmol
+ */
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
+public class UserVerifier {
+
+    private String filename;
+
+    public UserVerifier(String filename) {
+        this.filename = filename;
+    }
+
+    public boolean verifyUser(String userID, String password) {
+        try {
+            BufferedReader reader = new BufferedReader(new FileReader(filename));
+            String line;
+            while ((line = reader.readLine()) != null) {
+                String[] parts = line.split(",");
+                if (parts.length == 2 && parts[0].equals(userID) && parts[1].equals(password)) {
+                    return true;
+                }
+            }
+            reader.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return false;
+    }
+}
+
